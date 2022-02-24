54b8f56875d2dbb7b1d51eb2396a6008b9c8892f
blockjb <blockjb@umich.edu>
2022-02-24 16:10:10 -0500

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    // 0 for background Color(Black)
+    // A for text color(Green)
+    system("Color 0A");
+  
+    // Print any message
+    cout << "Geeks For Geeks!";
+    return 0;
+}
